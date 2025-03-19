29eeb9f2a00805d0229f4386e417bda981c1495b
MatheusMenck <matheushmenck@gmail.com>
2025-03-19 18:22:15 -0300

    1.1.1

--- resources/views/listar-animais.blade.php
+++ resources/views/listar-animais.blade.php
@@ -1,67 +1,8 @@
-<!doctype html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Animais para Adoção - Adote um Amigo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-      body {
-        background-color: #C4EEF2; /* Cor de fundo da página */
-      }
-      .animal-header {
-        background-color: #025159; /* Cor do cabeçalho */
-        color: #FFFFFF;
-        padding: 20px 0;
-        text-align: center;
-        margin-bottom: 30px;
-      }
-      .animal-header h1 {
-        margin: 0;
-        font-size: 2.5rem;
-      }
-      .animal-card {
-        background-color: #FFFFFF;
-        border-radius: 10px;
-        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-        margin-bottom: 20px;
-        overflow: hidden;
-      }
-      .animal-card img {
-        width: 100%;
-        height: 200px;
-        object-fit: cover;
-      }
-      .animal-card-body {
-        padding: 15px;
-      }
-      .animal-card-title {
-        color: #025159; /* Cor do título do card */
-        font-size: 1.25rem;
-        margin-bottom: 10px;
-      }
-      .animal-card-text {
-        color: #3F858C; /* Cor do texto do card */
-        font-size: 0.9rem;
-      }
-      .btn-details {
-        background-color: #A67458; /* Cor do botão */
-        border: none;
-        width: 100%;
-        padding: 10px;
-        border-radius: 5px;
-        color: #FFFFFF;
-        font-size: 16px;
-        margin-top: 10px;
-      }
-      .btn-details:hover {
-        background-color: #7AB8BF; /* Cor do botão ao passar o mouse */
-      }
-    </style>
-  </head>
-  <body>
-    <!-- Cabeçalho -->
-    <div class="animal-header">
+@extends('_partials/main')
+
+@section('conteudo')
+ <!-- Cabeçalho -->
+ <div class="animal-header">
       <h1>Animais para Adoção</h1>
     </div>
 
@@ -143,5 +84,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
+
+
