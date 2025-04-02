1a3e27797ef3348955401a255b820c52ad5dd493
Sthee2004 <viluvioletta182@gmail.com>
2025-04-02 18:23:09 -0300

    aula 02/04

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,12 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Inicio::class, 'Inicio'])->name('pagina-inicial');
-Route::get('/adocao', [App\Http\Controllers\Adocao::class, 'adocao'])->name('pagina-adocao');
-Route::get('/detalhes-animal', [App\Http\Controllers\DetalhesAnimal::class, 'detalhesAnimal'])->name('pagina-detalhes-animal');
-Route::get('/listar-animais', [App\Http\Controllers\ListarAnimais::class, 'listarAnimais'])->name('pagina-listar-animais');
-Route::get('/login', [App\Http\Controllers\Login::class, 'login'])->name('pagina-login');
-Route::get('/registro', [App\Http\Controllers\Registro::class, 'registro'])->name('pagina-registro');
-Route::get('/detalhes', [App\Http\Controllers\DetalhesAnimal::class, 'detalhesAnimal'])->name('pagina-registro');
+Route::get('/', [App\Http\Controllers\Inicio::class, 'Inicio'])->name('inicial');
+Route::get('/adocao', [App\Http\Controllers\Adocao::class, 'adocao'])->name('adocao');
+Route::get('/detalhes-animal', [App\Http\Controllers\DetalhesAnimal::class, 'detalhesAnimal'])->name('detalhes-animal');
+Route::get('/listar-animais', [App\Http\Controllers\ListarAnimais::class, 'listarAnimais'])->name('listar-animais');
+Route::get('/login', [App\Http\Controllers\Login::class, 'login'])->name('login');
+Route::get('/registro', [App\Http\Controllers\Registro::class, 'registro'])->name('registro');
+Route::get('/detalhes', [App\Http\Controllers\DetalhesAnimal::class, 'detalhesAnimal'])->name('registro');
 
-Route::post('/registro', [App\Http\Controllers\Registro::class, 'criarConta'])->name('pagina-registro');
-
-
-
+Route::post('/registro', [App\Http\Controllers\Registro::class, 'criarConta'])->name('registro');
